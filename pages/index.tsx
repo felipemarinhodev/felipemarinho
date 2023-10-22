cc9096ff2b12b1c167e19f6c458e64d1478bbe01
Felipe Marinho <github@felipemarinho.dev>
2023-10-21 22:12:37 -0700

    fetch todos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
+import { todoController } from "@ui/controller/todo";
 import { GlobalStyles } from "@ui/theme/GlobalStyles";
+import { useEffect, useState } from "react";
 const bg = "https://mariosouto.com/cursos/crudcomqualidade/bg";
 
+interface HomeTodo {
+  id: string;
+  content: string;
+}
 function HomePage() {
+  const [todos, setTodos] = useState<HomeTodo[]>([]);
+
+  useEffect(() => {
+    todoController.get().then((todos) => {
+      setTodos(todos);
+    });
+  }, []);
+
   return (
     <main>
       <GlobalStyles themeName="coolGrey" />
@@ -39,24 +53,20 @@ function HomePage() {
           </thead>
 
           <tbody>
-            <tr>
-              <td>
-                <input type="checkbox" />
-              </td>
-              <td>d4f26</td>
-              <td>
-                Conteúdo de uma TODO Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Eaque vero facilis obcaecati, autem aliquid
-                eius! Consequatur eaque doloribus laudantium soluta optio odit,
-                provident, ab voluptates doloremque voluptas recusandae
-                aspernatur aperiam.
-              </td>
-              <td align="right">
-                <button data-type="delete">Apagar</button>
-              </td>
-            </tr>
+            {todos.map((currentTodo) => (
+              <tr key={currentTodo.id}>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td>{currentTodo.id.substring(0, 4)}</td>
+                <td>{currentTodo.content}</td>
+                <td align="right">
+                  <button data-type="delete">Apagar</button>
+                </td>
+              </tr>
+            ))}
 
-            <tr>
+            {/* <tr>
               <td colSpan={4} align="center" style={{ textAlign: "center" }}>
                 Carregando...
               </td>
@@ -83,7 +93,7 @@ function HomePage() {
                   </span>
                 </button>
               </td>
-            </tr>
+            </tr> */}
           </tbody>
         </table>
       </section>
