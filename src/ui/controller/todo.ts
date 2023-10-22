5f55c7226a726e43753e671f071157a7f08ec86b
Felipe Marinho <github@felipemarinho.dev>
2023-10-22 13:40:09 -0700

    hide the load more button when don't have more todos and include todo in the todo list when it is fetch

--- src/ui/controller/todo.ts
+++ src/ui/controller/todo.ts
@@ -1,10 +1,10 @@
 import { todoRespository } from "@ui/repository/todo";
 
 interface TodoControllerGetParams {
-  page?: number;
+  page: number;
 }
 
-async function get({ page }: TodoControllerGetParams = {}) {
+async function get({ page }: TodoControllerGetParams) {
   return todoRespository.get({
     page: page || 1,
     limit: 1,
