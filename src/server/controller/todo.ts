e6b21fc58d0590f7aa1801b12d076c20545bcc51
Felipe Marinho <github@felipemarinho.dev>
2023-10-21 17:27:53 -0700

    create a controller file

---
+++ src/server/controller/todo.ts
@@ -0,0 +1,11 @@
+import { read } from "@db-crud-todo";
+import { NextApiRequest, NextApiResponse } from "next";
+
+function get(_: NextApiRequest, response: NextApiResponse) {
+  const ALL_TODOS = read();
+  response.status(200).json({ todos: ALL_TODOS });
+}
+
+export const todoController = {
+  get,
+};
