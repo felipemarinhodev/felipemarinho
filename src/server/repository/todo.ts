6404c7be6101c2d08df5d0f52a13e3aa7fac57eb
Felipe Marinho <github@felipemarinho.dev>
2023-10-23 16:56:45 -0700

    add function the toggleDone

--- src/server/repository/todo.ts
+++ src/server/repository/todo.ts
@@ -1,4 +1,4 @@
-import { create, read } from "@db-crud-todo";
+import { create, read, updateDoneById } from "@db-crud-todo";
 
 interface TodoRepositoryGetParams {
   page?: number;
@@ -38,7 +38,18 @@ async function createByContent(content: string): Promise<Todo> {
   return newTodo;
 }
 
+async function toggleDone(id: string): Promise<Todo> {
+  const ALL_TODOS = read();
+  const todo = ALL_TODOS.find((todo) => todo.id === id);
+  if (!todo) {
+    throw new Error(`Todo with id: "${id}" not found.`);
+  }
+  const updatedTodo = updateDoneById(todo.id, !todo.done);
+  return updatedTodo;
+}
+
 export const todoRepository = {
   get,
   createByContent,
+  toggleDone,
 };
