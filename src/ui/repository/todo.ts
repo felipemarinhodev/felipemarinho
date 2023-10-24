cbe9a5f3411cd82ff418ed57a239c44b96bd2046
Felipe Marinho <github@felipemarinho.dev>
2023-10-23 18:45:50 -0700

    add function toggleDone to update the attribute done

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -52,9 +52,30 @@ async function createByContent(content: string): Promise<Todo> {
   throw new Error("Failed to create TODO");
 }
 
+async function toggleDone(id: string): Promise<Todo> {
+  const response = await fetch(`/api/todos/${id}/toggle-done`, {
+    method: "PUT",
+  });
+
+  if (response.ok) {
+    const serverResponse = await response.json();
+    const ServerResponseSchema = schema.object({
+      todo: TodoSchema,
+    });
+    const serverResponseParsed = ServerResponseSchema.safeParse(serverResponse);
+    if (!serverResponseParsed.success) {
+      throw new Error(`Failed to update TODO with id ${id}`);
+    }
+    const updatedTodo = serverResponseParsed.data.todo;
+    return updatedTodo;
+  }
+  throw new Error("Server Error");
+}
+
 export const todoRepository = {
-  get,
   createByContent,
+  get,
+  toggleDone,
 };
 
 function parseTodosFromServer(responseBody: unknown): {
