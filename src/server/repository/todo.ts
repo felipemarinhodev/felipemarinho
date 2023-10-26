a2d7c1f40490534d5805dc225cb640430d6b22c7
Felipe Marinho <github@felipemarinho.dev>
2023-10-26 07:58:44 -0700

    insert new todo in supabase

--- src/server/repository/todo.ts
+++ src/server/repository/todo.ts
@@ -52,8 +52,20 @@ async function get({
 }
 
 async function createByContent(content: string): Promise<Todo> {
-  const newTodo = create(content);
-  return newTodo;
+  const { data, error } = await supabase
+    .from("todos")
+    .insert([
+      {
+        content,
+      },
+    ])
+    .select()
+    .single();
+
+  if (error) throw new Error("Failed to create todo");
+
+  const parsedData = TodoSchema.parse(data);
+  return parsedData;
 }
 
 async function toggleDone(id: string): Promise<Todo> {
